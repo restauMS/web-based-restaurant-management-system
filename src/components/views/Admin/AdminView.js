5d0578a84e4ba4dfd00fe3cc18c01b21037b8dc1
lozanasc <lozanasc@gmail.com>
2020-11-23 10:07:29 +0800

    Added class component for Login --subject to change

--- src/components/views/Admin/AdminView.js
+++ src/components/views/Admin/AdminView.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import AdminLayout from '../../layouts/Admin/AdminLayout';
 
+class Login extends React.Component{
+    // Login stuff goes here
+}
 export default class AdminView {
     render(){
         return (
