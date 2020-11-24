882aeeac4a76592036e50024000be9b5363ecccf
lozanasc <lozanasc@gmail.com>
2020-11-24 21:06:48 +0800

    Removed comment on previous function params, refactored SetCardType method

--- src/components/common/Card/Card.js
+++ src/components/common/Card/Card.js
@@ -48,43 +48,38 @@ function FoodItemCardComponent(props){
         )
 }
 
-/*
- @param PropCardType 
- * Pass in a String indicating what type of Card are we trying to Render
- ? returns a Sub Component
-
- @param SubPropObject
- * Pass in an Object that will contain Props for the Sub Components
- ? returns an Object
-*/
-function SetCardType(PropCardType, SubPropObject){
-    switch(PropCardType){
+// Todo: Update Comment
+function SetCardType(CardType){
+    switch(CardType.TypeOfCard){
         case 'Alert': {
             return <AlertCardComponent
-                    PropObject={SubPropObject}
+                    PropObject={CardType.SubCardProps}
                     />;
         }
         case 'OrderNote': {
             return <OrderNoteCardComponent
-                    PropObject={SubPropObject}
+                    PropObject={CardType.SubCardProps}
                     />;
         }
         case 'TableCard': {
             return <TableCardComponent
-                    PropObject={SubPropObject}
+                    PropObject={CardType.SubCardProps}
                     />;
         }
         case 'FoodCard': {
             return <FoodItemCardComponent
-                    PropObject={SubPropObject}
+                    PropObject={CardType.SubCardProps}
                     />
         }
         default:
             return <h1>Something definitely went wrong :C</h1>
     }
 }
+/*
+    @param props
 
-export default function Card(props) {
+*/
+export default function Card(CardType) {
         // Funny code tbh this is very spaghetti lol xd, hardly scalable but it works nonetheless
-        return SetCardType(props.CardType, props.SubPropObject);
+        return SetCardType(CardType);
 }
