d1e9d3548b4cc19d031be3cd404285363b549f79
lozanasc <lozanasc@gmail.com>
2020-12-17 22:11:26 +0800

    Converted class to functional component & Refactored redundant sass code

--- src/components/common/Button/Button.js
+++ src/components/common/Button/Button.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import './style/Button.scss';
 
-export default class Button extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            // State stuff goes here
-        }
-        // Binding the handlerChange to be exclusive only to a particular instance
-        this.handlerChange = this.handlerChange.bind(this);
-    }
-
-    handlerChange(){
-        // Some stuff happens here
-    }
-
-    render(){
-        return (<button className={this.props.isContrast ? 'Button-Contrast' : 'Button'}>
-            {this.props.ButtonText}
-        </button>)
-    }
+const Button = props => {
+    return (
+        <button 
+        onClick={() => props.buttonFunction}
+        className="Button -Contrast">
+            {props.ButtonText}
+        </button>
+    )
 }
+
+export default Button;
