7d6ffe427f6eb78d1d7828335fd78e4a7fbe2587
lozanasc <lozanasc@gmail.com>
2021-01-02 21:31:37 +0800

    Added LogData hook for Logged data from CustomerForm

--- src/components/views/Customer/CustomerView.js
+++ src/components/views/Customer/CustomerView.js
@@ -2,23 +2,30 @@ import React, {useState} from 'react';
 
 // Customer Logger View
 import CustomerLog from './CustomerLog/CustomerLog';
+import CustomerMenu from './CustomerMenu/CustomerMenu';
 
 const CustomerView = () => {
 
         const [Stage, SetStage] = useState(1);
         
+        const [LogData, SetLogData] = useState({});
+
         // eslint-disable-next-line
         const _nextStage = () => {
                 SetStage(Stage >= 1 ? 2 : Stage + 1);
         }
+        
         return (
                 <div>       
-                        {Stage !== 1 
-                                ? null : 
-                                <CustomerLog
-                                        Stage = {_nextStage}
-                                />
-                        }
+                        <CustomerLog
+                                NextStage = {_nextStage}
+                                Stage = {Stage}
+                                LogData = {SetLogData}
+                        />
+                        <CustomerMenu
+                                Stage = {Stage}
+                                LoggedData = {LogData}
+                        />
                 </div>
         );
 }
