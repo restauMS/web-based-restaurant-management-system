56c760b19b2eca98266c2b3a206494e34925149e
lozanasc <lozanasc@gmail.com>
2021-01-02 21:39:04 +0800

    Added CustomerPolicyAgreement component for Policy and Data Usage Agreement *TestData.json for Testing Purposes

---
+++ src/components/views/Customer/CustomerLog/CustomerPolicyAgreement.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CustomerPolicyAgreement = props => {
+    return (
+        <div>
+            {/* Customer Policy Agreement code goes here retardo */}
+        </div>
+    );
+}
+
+export default CustomerPolicyAgreement;
