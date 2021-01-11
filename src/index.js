0e66b3f23bfec3dd866103dc9dc8b0302470a6a5
lozanasc <lozanasc@gmail.com>
2021-01-11 19:44:07 +0800

    Started added Routes for Starter page that will act as the installer for the application

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Component Styling
 import './index.scss';
+// Different View Layouts that holds each Service Interface Component
 import Customer from './components/layouts/Customer/CustomerLayout';
-
+import Admin from './components/layouts/Admin/AdminLayout';
+import Worker from './components/layouts/Worker/WorkerLayout';
+import Starter from './components/views/Installation/Installation';
 // Root Element of the Application
 const RootElement = document.getElementById('root');
 // Test Component Render
 ReactDOM.render(
     <div className="ContainerIndex">
-        <Customer/>
+        <Starter
+            CustomerService = {Customer}
+            AdminService = {Admin}
+            WorkerService = {Worker}
+        />
     </div>,
     RootElement
 );
