8a0282547f0dde949f9c23d250831dd8e88828f4
lozanasc <lozanasc@gmail.com>
2021-01-12 22:00:34 +0800

    Completed NotFound component that would serve as /404 page

--- src/components/views/NotFound/NotFound.js
+++ src/components/views/NotFound/NotFound.js
@@ -1,2 +1,28 @@
 import React from 'react';
+
+// Component Imports
+import Label from '../../common/Label/Label';
+import Button from '../../common/Button/Button';
+// Stylesheet Import
 import './style/NotFound.scss';
+
+const NotFound = props => {
+    return (
+        <div className="NotFound">
+            <div className="Info">
+                <Label
+                LabelContent = 'Sorry, this page does not exist.'
+                isLabelContrast = {false}
+                />
+                <Button
+                ButtonContent = 'exit.'
+                isButtonContrast = {true}
+                isButtonLink = {true}
+                ButtonLinkRoute = '/'
+                />
+            </div>
+        </div>
+    );
+}
+
+export default NotFound;
