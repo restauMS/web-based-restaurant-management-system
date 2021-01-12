0f24d4c04526840288d422b212f8b4f3f499c48b
lozanasc <lozanasc@gmail.com>
2021-01-12 21:53:41 +0800

    Added NotFound component for /404 route

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // Component Styling
 import './index.scss';
-// Different View Layouts that holds each Service Interface Component
+// Different View Layouts components that holds each Service Interface Component
 import Customer from './components/layouts/Customer/CustomerLayout';
 import Admin from './components/layouts/Admin/AdminLayout';
 import Worker from './components/layouts/Worker/WorkerLayout';
 import Starter from './components/views/Installation/Installation';
+import NotFound from './components/views/NotFound/NotFound';
 // Root Element of the Application
 const RootElement = document.getElementById('root');
 // Test Component Render
@@ -16,6 +17,7 @@ ReactDOM.render(
             CustomerService = {Customer}
             AdminService = {Admin}
             WorkerService = {Worker}
+            NotFound = {NotFound}
         />
     </div>,
     RootElement
