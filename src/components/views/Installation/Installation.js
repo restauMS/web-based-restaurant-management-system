6f2ea9d7408b519c6e05ee585d9fb0039a77f5f0
lozanasc <lozanasc@gmail.com>
2021-01-12 21:59:57 +0800

    Completed main page as installation/selection page

--- src/components/views/Installation/Installation.js
+++ src/components/views/Installation/Installation.js
@@ -1,49 +1,74 @@
-// ! The installation component will only be for the Client devices and is for **TESTING ONLY!**
+// ! TESTING ONLY
 import React from 'react';
+import { Spring } from 'react-spring/renderprops';
 import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+// Component Imports
+import LinkButton from '../../common/Button/Button';
+import Label from '../../common/Label/Label';
+// Asset Imports
+import RestoMSLogo from '../../../assets/restoms-logo/logo.png';
 import './style/Installation.scss';
 
+
+
+
+const Selection = () => {
+    return (
+        <Spring
+        from = {{opacity: 0, transition: '0.1s ease-in-out'}}
+        to = {{opacity: 1}}
+        >
+            {props => 
+                <div className='Installation' style={{...props}}>
+                    <div className="SelectContainer">
+                        <img src={RestoMSLogo} alt="RestoMS Logo" id="restoms-logo"/>
+                        <Label
+                            LabelContent = {`Service Menu`}
+                        />
+                        <LinkButton
+                            isButtonLink = {true}
+                            ButtonContent = 'Customer'
+                            isButtonContrast = {true}
+                            ButtonLinkRoute = '/customer'
+                        />
+                        <LinkButton
+                            isButtonLink = {true}
+                            ButtonContent = 'Admin'
+                            isButtonContrast = {true}
+                            ButtonLinkRoute = '/admin'
+                        />
+                        <LinkButton
+                            isButtonLink = {true}
+                            ButtonContent = 'Worker'
+                            isButtonContrast = {true}
+                            ButtonLinkRoute = '/worker'
+                        />
+                    </div>
+                </div>
+            }
+        </Spring>
+    )
+}
+
 /**
- * 
  * @param {Component} props.CustomerService 
  * To render Customer view service that is intended for Customers
  * @param {Component} props.AdminService
- * To render Cu
+ * To render Admin view service intended for the Admin
  * @param {Component} props.WorkerService
+ * To render Worker view service inteded for the Workers
  */
 const Installation = props =>{
 return (
     <Router>
-        <div className="Installation">
-            <ul>
-                <li>
-                <Route path = '/customer'>
-                    {props.CustomerService}
-                </Route>
-                </li>
-                <li>
-                    <Redirect to = '/admin'>
-                        Admin
-                    </Redirect>
-                </li>
-                <li>
-                    <Redirect to = '/worker'>
-                        Worker
-                    </Redirect>
-                </li>
-            </ul>
-            {/* <Switch>
-                <Route path = '/customer'>
-                    {props.CustomerService}
-                </Route>
-                <Route path = '/admin'>
-                    {props.AdminService}
-                </Route>
-                <Route path='/worker'>
-                    {props.WorkerService}
-                </Route>
-            </Switch> */}
-        </div>
+            <Switch>
+                <Route exact path = '/' component={Selection}/>
+                <Route exact path = '/customer' component={props.CustomerService}/>
+                <Route exact path = '/admin' component={props.AdminService}/>
+                <Route exact path='/worker' component={props.WorkerService}/>
+                <Route exact path='/404' component={props.NotFound}/>
+                <Redirect to='/404'/>
+            </Switch>
     </Router>
 );
 }
