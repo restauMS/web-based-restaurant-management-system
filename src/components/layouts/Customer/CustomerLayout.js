9c10706240ba21e8b71f906e45227eed6312fa40
lozanasc <lozanasc@gmail.com>
2021-01-12 21:55:45 +0800

    Added transition animation for Customer View component

--- src/components/layouts/Customer/CustomerLayout.js
+++ src/components/layouts/Customer/CustomerLayout.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Spring } from 'react-spring/renderprops';
 import './style/CustomerLayout.scss';
 import CustomerView from '../../views/Customer/CustomerView';
 
 export default function CustomerLayout(){
     return (
-        <div className="CustomerLayoutContainer">
-            <CustomerView/>
-        </div>
+        <Spring
+        from = {{opacity: 0, transition: '0.1s ease-in-out'}}
+        to = {{opacity: 1}}
+        >
+            {
+                props => 
+                <div className="CustomerLayoutContainer" style={{...props}}>
+                    <CustomerView/>
+                </div>
+            }
+        </Spring>
     )
 }
