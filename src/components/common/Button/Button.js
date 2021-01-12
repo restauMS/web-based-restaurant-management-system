bbe23ec8a08e95a5127bf405577d8079d2608408
lozanasc <lozanasc@gmail.com>
2021-01-12 21:53:09 +0800

    Added LinkButton functionality in the Button component fixed append style props

--- src/components/common/Button/Button.js
+++ src/components/common/Button/Button.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import './style/Button.scss';
+import {Link} from 'react-router-dom';
 
 const Button = props => {
-    return (
+    return (props.isButtonLink ? (
+    <Link 
+        className={props.isButtonContrast ? 'Button ButtonContrast Link' : 'Button Link'}
+        role="button"
+        style = {{...props.Style}}
+        to={props.ButtonLinkRoute}
+        onClick={props.ButtonFunction}
+        > 
+        {props.ButtonContent}
+    </Link>
+    )
+    :
+    (
         <button 
         onClick={props.ButtonFunction}
         className={props.isButtonContrast ? 'Button ButtonContrast' : 'Button'}
-        style = {props.Style}
+        style = {{...props.Style}}
         >
             {props.ButtonContent}
-        </button>
+        </button>)
     )
 }
 
