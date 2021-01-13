ad8244f0c3aa0e9d57ed9f8b3176b4fe676ed45e
lozanasc <lozanasc@gmail.com>
2021-01-13 21:37:10 +0800

    Formatted Button.js code and refacted styling for Button

--- src/components/common/Button/Button.js
+++ src/components/common/Button/Button.js
@@ -3,27 +3,29 @@ import './style/Button.scss';
 import {Link} from 'react-router-dom';
 
 const Button = props => {
-    return (props.isButtonLink ? (
-    <Link 
-        className={props.isButtonContrast ? 'Button ButtonContrast Link' : 'Button Link'}
-        role="button"
-        style = {{...props.Style}}
-        to={props.ButtonLinkRoute}
-        onClick={props.ButtonFunction}
-        > 
-        {props.ButtonContent}
-    </Link>
-    )
-    :
-    (
-        <button 
-        onClick={props.ButtonFunction}
-        className={props.isButtonContrast ? 'Button ButtonContrast' : 'Button'}
-        style = {{...props.Style}}
-        >
-            {props.ButtonContent}
-        </button>)
-    )
+    return (
+        props.isButtonLink ? 
+            (
+                <Link 
+                    className={props.isButtonContrast ? 'Button ButtonContrast Link' : 'Button Link'}
+                    role="button"
+                    style = {{...props.Style}}
+                    to={props.ButtonLinkRoute}
+                    onClick={props.ButtonFunction}
+                    > 
+                    {props.ButtonContent}
+                </Link>
+            )
+        :
+            (
+                <button 
+                onClick={props.ButtonFunction}
+                className={props.isButtonContrast ? 'Button ButtonContrast' : 'Button'}
+                style = {{...props.Style}}
+                >
+                    {props.ButtonContent}
+                </button>)
+            );
 }
 
 export default Button;
