0c58439a4a5815dde0490e9053388fefd092910a
lozanasc <lozanasc@gmail.com>
2021-01-23 23:38:41 +0800

    Customer Routes updated: Added Fetching Table data and fixed Fetch Available Table Data service

--- routes/customer/routes.customer.js
+++ routes/customer/routes.customer.js
@@ -4,4 +4,23 @@ const Router = express.Router();
 // Services
 const FetchTableData = require('../../services/customer/customer.fetchAvailableTable');
 
+
+Router.post('/', async(Request, Response) => {
+    try {
+        const Data = await FetchTableData();
+        if(Data){
+            Response.status(200)
+            .send(Data);
+        }
+        else {
+            Response.status(500)
+            .send({
+                'Status': 'Failed'
+            });
+        }
+    } catch (error) {
+        console.trace(error);
+    }
+});
+
 module.exports = Router;
--- services/customer/customer.fetchAvailableTable.js
+++ services/customer/customer.fetchAvailableTable.js
@@ -2,10 +2,11 @@ const Connection = require('../../database/mysql.connection');
 
 module.exports = async() => {
     try {
-        const Query = `SELECT * FROM student.tablelist`;
+        const Query = ``;
         const TableData = await Connection(Query);
         return TableData.json();
     } catch (error) {
         console.trace(error);
+        return [];
     }
 }
