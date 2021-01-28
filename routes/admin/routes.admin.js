fec26054d5db91dbaa68378231772ef28f79cdde
lozanasc <lozanasc@gmail.com>
2021-01-28 09:09:41 +0800

    Added auth route for admin

--- routes/admin/routes.admin.js
+++ routes/admin/routes.admin.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const Router = express.Router();
 
-
 // Services
+const Authenticate = require('../../services/admin/admin.auth');
+
+Router.post('/Authenticate', async(Request, Response) => {
+        try {
+            const { Username } = Request.body;
+            const Auth = await Authenticate(Username);
+            if (Auth) {
+                Response.status(200)
+                .send(Auth);
+            } else {
+                Response.status(500)
+                .send({
+                    "Status": Auth,
+                    "StatusDescription": "Username not found, operation failed."
+                });
+            }
+
+        } catch (error) {
+            console.trace(error);
+        }
+});
+
 module.exports = Router;
