ffa08b056259344ad4f24f299db5102ddd7d9e83
lozanasc <lozanasc@gmail.com>
2021-02-03 22:23:46 +0800

    Added FetchOrderQueue service and route

--- routes/worker/routes.worker.js
+++ routes/worker/routes.worker.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const Router = express.Router();
+
 // Services
 const Authenticate = require('../../services/worker/worker.auth');
+const FetchOrderQueue = require('../../services/worker/worker.fetchOrderQueue');
+
 
 // ! Very bad smelly authentication
 Router.post('/Authenticate', async(Request, Response) => {
@@ -15,9 +18,29 @@ Router.post('/Authenticate', async(Request, Response) => {
             Response.status(500)
             .send({
                 "Status": Auth,
-                "StatusDescription": "Username not found, operation failed"
+                "StatusDescription": "Something went wrong, operation failed."
             });
         }
+    } catch (error) {
+        console.trace(error);
+    }
+});
+
+Router.post('/FetchOrderQueue', async(Request, Response) => {
+    try {
+        const OrderQueue = await FetchOrderQueue();
+        if(OrderQueue){
+            Response.status(200)
+            .send(OrderQueue)
+        }
+        else {
+            Response.status(500)
+            .send({
+                'Status': OrderQueue,
+                'StatusDescpription': 'Something went wrong, operation failed.'
+            })
+        }
+
     } catch (error) {
         console.trace(error);
     }
