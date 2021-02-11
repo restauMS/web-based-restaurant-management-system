7ecc8e56ef47c9d77acc0fcf19a53fb6b2121d82
lozanasc <lozanasc@gmail.com>
2021-02-11 18:56:58 +0800

    Added necessary comments for parameter descriptions

--- services/customer/customer.pushLogData.js
+++ services/customer/customer.pushLogData.js
@@ -1,20 +1,29 @@
 const Connection = require('../../database/mysql.connection');
 /**
  * 
- * @param {String} CustomerName
- * @param {String} CustomerContact 
- * @param {String} CustomerAddress 
- * @param {String} CustomerTable 
+ * @param {String} CustomerName Customer's name
+ * @param {String} CustomerContact Customer's contact number
+ * @param {String} CustomerAddress Customer's physical address
+ * @param {String} CustomerTable Customer's table of choice
  */
 module.exports = async(CustomerName, CustomerContact, CustomerAddress, CustomerTable) => {
     try {
-        const Query =   `INSERT INTO customer_log (log_id,log_name,log_contact_no,log_address,log_table) 
+        const Query =   `INSERT INTO customer_log 
+                        (
+                            log_id,
+                            log_name,
+                            log_contact_no,
+                            log_address,
+                            log_table
+                        ) 
                         VALUES 
-                        (null, 
-                        '${CustomerName}', 
-                        '${CustomerContact}', 
-                        '${CustomerAddress}', 
-                        '${CustomerTable}');`
+                        (
+                            null, 
+                            '${CustomerName}', 
+                            '${CustomerContact}', 
+                            '${CustomerAddress}', 
+                            '${CustomerTable}'
+                        );`
         await Connection(Query);
         return true;
     } catch (error) {
