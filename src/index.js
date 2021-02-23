9302e78e70ab6b3eb96543f7c8b256fcafd003be
lozanasc <lozanasc@gmail.com>
2021-02-23 17:20:13 +0800

    Routing transferred to Master component than index
    Signed-off-by: lozanasc <lozanasc@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
+// Dependency Imports
 import React from 'react';
 import ReactDOM from 'react-dom';
-// Component Styling
+
+// Component Styling Imports
 import './index.scss';
-// Different View Layouts components that holds each Service Interface Component
-import Customer from './components/layouts/Customer/CustomerLayout';
-import Admin from './components/layouts/Admin/AdminLayout';
-import Worker from './components/layouts/Worker/WorkerLayout';
-import Starter from './components/views/Installation/Installation';
-import NotFound from './components/views/NotFound/NotFound';
+
+// Component Imports
+import App from './components/views/Master/Master';
+
 // Root Element of the Application
 const RootElement = document.getElementById('root');
 // Test Component Render
 ReactDOM.render(
     <div className="ContainerIndex">
-        <Starter
-            CustomerService = {Customer}
-            AdminService = {Admin}
-            WorkerService = {Worker}
-            NotFound = {NotFound}
-        />
+        <App/>
     </div>,
     RootElement
 );
