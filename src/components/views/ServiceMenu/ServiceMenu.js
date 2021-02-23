ce5e2f8513bff2d25cba1a7ddfbf60e36bd61775
lozanasc <lozanasc@gmail.com>
2021-02-23 17:52:25 +0800

    Added: ServiceMenu * Added ServiceMenu react component * Added styling for the ServiceMenu component Will serve as the main interface for choosing the right service component to mount
    Signed-off-by: lozanasc <lozanasc@gmail.com>

---
+++ src/components/views/ServiceMenu/ServiceMenu.js
@@ -0,0 +1,54 @@
+// Dependency Imports
+import React from 'react'
+import { Spring } from 'react-spring/renderprops';
+
+// Component Imports
+import LinkButton from '../../common/Button/Button';
+import Label from '../../common/Label/Label';
+
+// Asset Imports
+import RestoMSLogo from '../../../assets/restoms-logo/logo.png';
+
+// Component Styling Import
+import './style/ServiceMenu.scss';
+
+const ServiceMenu = ({match}) => {
+    return (
+        <Spring
+            from = {{opacity: 0, transition: '0.1s ease-in-out'}}
+            to = {{opacity: 1}}
+        >   
+            {
+                props => 
+                <div className="ServiceMenu" style={{...props}}>
+                    <div className="SelectContainer">
+                        <img src={RestoMSLogo} alt="System Logo" id="restoms-logo"/>
+                        <Label
+                            LabelContent = 'Service Menu'
+                        />
+                        <LinkButton
+                            isButtonLink = {true}
+                            ButtonContent = 'Customer'
+                            isButtonContrast = {true}
+                            ButtonLinkRoute = '/Customer'
+                        />
+                        <LinkButton
+                            isButtonLink = {true}
+                            ButtonContent = 'Admin'
+                            isButtonContrast = {true}
+                            ButtonLinkRoute = ''
+                        />
+                        <LinkButton
+                            isButtonLink = {true}
+                            ButtonContent = 'Worker'
+                            isButtonContrast = {true}
+                            ButtonLinkRoute = '/Worker/Login'
+                        />
+                    </div>
+                </div>
+            }
+        </Spring>
+    )
+}
+
+export default ServiceMenu;
