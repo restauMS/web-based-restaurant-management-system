5e3ad51f1f822436a230bf40c18c2b35b141f482
lozanasc <lozanasc@gmail.com>
2021-03-02 22:13:30 +0800

    Added component import for Admin in Master
    Signed-off-by: lozanasc <lozanasc@gmail.com>

--- src/components/views/Master/Master.js
+++ src/components/views/Master/Master.js
@@ -5,10 +5,13 @@ import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom
 // Component Imports
 import NotFound from '../NotFound/NotFound';
 import ServiceMenu from '../ServiceMenu/ServiceMenu';
-// ? Customer Component Imports
+// ? Customer Component Import
 import Customer from '../../layouts/Customer/CustomerLayout';
-// ? Worker Component Imports
+// ? Worker Component Import
 import Worker from '../../layouts/Worker/WorkerLayout';
+// ? Admin Component Import
+import Admin from '../../layouts/Admin/AdminLayout';
+
 
 const Master = () => {
 return (
@@ -18,6 +21,7 @@ return (
                 {/* Complete parent and child routes for each service component */}
                 { Customer.map(({path, component}, key) => <Route exact strict path = {path} component = {component} key = {key} />)}
                 { Worker.map(({path, component}, key) => <Route exact strict path = {path} component = {component} key = {key} />)}
+                { Admin.map(({path, component}, key) => <Route exact strict path = {path} component = {component} key = {key} />) }
                 <Route exact path='/404' component={NotFound}/>
                 <Redirect to='/404'/>
             </Switch>
