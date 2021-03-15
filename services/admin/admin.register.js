d7691993a338c6f602ab4cfa734d23c622e20649
Sean Christian Lozana <lozanasc@gmail.com>
2021-03-15 12:06:04 +0800

    Added documentation for Registration service

--- services/admin/admin.register.js
+++ services/admin/admin.register.js
@@ -1,5 +1,14 @@
 const Connection = require('../../database/mysql.connection');
 
+/**
+ * 
+ * @param {String} Username 
+ * @param {String} Password 
+ * @param {String} FullName 
+ * @param {String} Contact 
+ * @param {String} Address 
+ * @returns Result (true/false) of the query if it was successful or not
+ */
 module.exports = async(Username, Password, FullName, Contact, Address) => {
     try {
         await Connection(`INSERT INTO admin 
