b3e8a3c6bd3d734326cb6a0714c0f606d083dcb0
Sean Christian Lozana <lozanasc@gmail.com>
2021-03-15 12:06:34 +0800

    Added Admin Sales service for GetSales Protected API testing

---
+++ services/admin/admin.sales.js
@@ -0,0 +1,14 @@
+/*
+    ? This is a protected service
+*/
+const Connection = require('../../database/mysql.connection');
+
+module.exports = async() => {
+    try {
+        const Sales = await Connection('SELECT * FROM SALES');
+        return Sales;
+    } catch (error) {
+        console.trace("Something went wrong: ", error);
+        return [];
+    }
+}
