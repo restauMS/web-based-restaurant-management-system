e6bab1edd30691cfd84b2d93a4ec9ab242d8f26d
Sean Christian Lozana <lozanasc@gmail.com>
2021-03-17 22:25:03 +0800

    Replacing old error message to fit actual error

--- src/components/views/NotFound/NotFound.js
+++ src/components/views/NotFound/NotFound.js
@@ -6,12 +6,12 @@ import Button from '../../common/Button/Button';
 // Stylesheet Import
 import './style/NotFound.scss';
 
-const NotFound = props => {
+const NotFound = () => {
     return (
         <div className="NotFound">
             <div className="Info">
                 <Label
-                LabelContent = {`Oh, you're not supposed to be here.`}
+                LabelContent = {`Oops! Page not found...`}
                 isLabelContrast = {false}
                 />
                 <Button
@@ -21,7 +21,7 @@ const NotFound = props => {
                 ButtonLinkRoute = '/'
                 />
             </div>
-        </div>
+        </div>  
     );
 }
 
