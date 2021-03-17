8253474f2590d7648acf81599ac19269c093e104
Sean Christian Lozana <lozanasc@gmail.com>
2021-03-17 22:23:08 +0800

    Testing on DashboardContent component

---
+++ src/components/views/Admin/AdminDashboard/components/AdminDashboardContent.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+// Component Imports
+import ContentLabel from '../../../../common/Label/Label';
+
+const AdminDashboardContent = (props) => {
+    return (
+        <div className="Content">
+            <div>
+                <ContentLabel
+                    isLabelContrast = {true}
+                    LabelContent = {`Hello, ${props.AdminName}`}
+                    Style = {{fontSize: '24px', textAlign: "start"}}
+                />
+                <ContentLabel
+                    isLabelContrast = {true}
+                    LabelContent = 'Dashboard'
+                    Style = {{textAlign: "start"}}
+                />
+                <ContentLabel
+                    isLabelContrast = {true}
+                    LabelContent = {`Authentication Type: ${props.AuthType}`}
+                    Style = {{fontSize: '24px', textAlign: "start"}}
+                />
+                <ContentLabel
+                    isLabelContrast = {true}
+                    LabelContent = {`Authentication Token: ${props.AuthToken}`}
+                    Style = {{fontSize: '8px', textAlign: "start"}}
+                />
+            </div>
+            
+        </div>
+    )
+}
+
+export default AdminDashboardContent;
