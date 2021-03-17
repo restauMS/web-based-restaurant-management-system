76d148e548eda65742aec63825b8a75cd60dd410
Sean Christian Lozana <lozanasc@gmail.com>
2021-03-17 22:19:51 +0800

    Dashboard button support for Navbars: * Added DashboardButton * Created styling for DashboardButton
    Signed-off-by: Sean Christian Lozana <lozanasc@gmail.com>

--- src/components/common/Button/Button.js
+++ src/components/common/Button/Button.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './style/Button.scss';
-import {Link} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 
 const Button = props => {
     return (
         props.isButtonLink ? 
             (
                 <Link 
-                    className={props.isButtonContrast ? 'Button ButtonContrast Link' : 'Button Link'}
+                    className={props.isButtonContrast ? 'Button ButtonContrast Link onActive' : 'Button Link onActive'}
                     role="button"
                     style = {{...props.Style}}
                     to={props.ButtonLinkRoute}
@@ -20,12 +20,31 @@ const Button = props => {
             (
                 <button 
                 onClick={props.ButtonFunction}
-                className={props.isButtonContrast ? 'Button ButtonContrast' : 'Button'}
+                className={props.isButtonContrast ? 'Button ButtonContrast onActive' : 'Button onActive'}
                 style = {{...props.Style}}
                 >
                     {props.ButtonContent}
-                </button>)
-            );
+                </button>
+            )
+    );
+}
+
+export const DashboardButton = props => {
+    return (
+                <NavLink
+                    className={props.isButtonContrast ? 'DashboardButton ButtonContrast onActive' : 'DashboardButton onActive'}
+                    activeStyle = {props.activeStyle}
+                    to = {props.Route}
+                    style = {{...props.Style}}
+                >
+                    <img 
+                        src={props.ButtonImageProps.src} 
+                        alt={props.ButtonImageProps.alt} 
+                        height={props.ButtonImageProps.height} 
+                        width = {props.ButtonImageProps.width}
+                    />
+                </NavLink>
+    );
 }
 
 export default Button;
