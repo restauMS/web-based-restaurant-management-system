38be9059c26e5826e4d7c173876536909ad85c5a
Sean Christian Lozana <lozanasc@gmail.com>
2021-03-30 10:56:32 +0800

    Added proper indentations for cleaner look

--- src/components/hooks/AuthCheck.js
+++ src/components/hooks/AuthCheck.js
@@ -1,20 +1,21 @@
 import { useState } from 'react';
 
 export const useAuth = () => {
-const getAuth = () => {
-const authStatus = localStorage.getItem('AuthStatus');
-return authStatus;
-};
+    
+    const getAuth = () => {
+        const authStatus = localStorage.getItem('AuthStatus');
+        return authStatus;
+    };
 
-const [AuthStatus, SetAuthStatus] = useState(getAuth());
+    const [AuthStatus, SetAuthStatus] = useState(getAuth());
 
-const saveAuthStatus = authStatus => {
-localStorage.setItem('AuthStatus', authStatus);
-SetAuthStatus(authStatus.AuthStatus);
-};
+    const saveAuthStatus = authStatus => {
+        localStorage.setItem('AuthStatus', authStatus);
+        SetAuthStatus(authStatus.AuthStatus);
+    };
 
-return {
-SetAuthStatus: saveAuthStatus,
-AuthStatus
-}
+    return {
+    SetAuthStatus: saveAuthStatus,
+    AuthStatus
+    }
 }
