9d0c6bbd6550c2555977b4ef3bd0f80ea3f8a5fd
Sean Christian Lozana <lozanasc@gmail.com>
2021-05-17 12:45:01 +0800

    Implemented Dashboard design for home and sub-routes and its components

--- src/components/views/Worker/WorkerDashboard/components/DashboardContent/WorkerDashboardContent.js
+++ src/components/views/Worker/WorkerDashboard/components/DashboardContent/WorkerDashboardContent.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import {Switch} from 'react-router-dom';
 
-const WorkerDashboardContent = () => {
+import Main from './Content/Home';
+import ControllerRouter from '../../../../../routers/Controller';
+import './style/Content.scss';
+
+const WorkerDashboardContent = ({routes, agent_info}) => {
+
+    const Home = window.location.pathname;
+
     return (
-        <div>
-            
+        <div className = "WorkerContent">
+            {
+                Home === '/Worker/Dashboard' ?
+                <Main
+                    info = {agent_info}
+                />
+                :
+                null
+            }
+            <Switch>
+                {
+                    routes.map((route, key) => 
+                        <ControllerRouter
+                            key = {key}
+                            {...route}
+                        />
+                    )
+                }   
+            </Switch>
         </div>
     )
 }
