b26c347d7a8b5c83f7ca5c909fcf5d4b03547112
Sean Christian Lozana <lozanasc@gmail.com>
2021-05-17 12:47:35 +0800

    Implemented design for the Worker Inventory dashboard content

---
+++ src/components/views/Worker/WorkerDashboard/components/DashboardContent/Content/Inventory.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Spring } from 'react-spring/renderprops';
+
+import Label from '../../../../../../common/Label/Label';
+import { ListCard } from '../../../../../../common/Card/Card';
+
+const Inventory = () => {
+    return (
+        <Spring
+        from = {{opacity: 0, transition: '0.1s ease-in-out'}}
+        to = {{opacity: 1}}
+        >
+            {
+                props => 
+                <div className="InventoryContainer" style={{...props}}>
+                    <div className="TopBar">
+                        <Label
+                            LabelContent = "Inventory"
+                            isLabelContrast = {true}
+                            Style = {{color: '#E56B6F'}}
+                        />
+                        <Label
+                            LabelContent = {`Hello, ${localStorage.getItem("Username")}`}
+                            Style = {{fontSize: 'clamp(20px, 30px, 40px)', color: '#E56B6F'}}
+                            isLabelContrast = {true}
+                        />
+                    </div>
+                    <div className="InventoryListContainer">
+                        <div className="InventoryList">
+                            {/* {
+                                WhateverListGoesHere.map((Items, key) => <ListCard key = {key} CardContent = {Items}/>)
+                            } */}
+                        </div>
+                    </div>
+                </div>
+            }
+        </Spring>
+    );
+}
+
+export default Inventory;
