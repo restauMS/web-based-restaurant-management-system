836b6b9f708fe56f65d8e8c2e2d0e7fdbf5e1dd2
Sean Christian Lozana <lozanasc@gmail.com>
2021-06-01 15:48:04 +0800

    Added setter function for initializing the Customer session status

--- src/components/views/Customer/CustomerLog/CustomerPolicyAgreement/CustomerPolicyAgreement.js
+++ src/components/views/Customer/CustomerLog/CustomerPolicyAgreement/CustomerPolicyAgreement.js
@@ -6,7 +6,7 @@ import { CustomerContext } from '../../../../contexts/CustomerContext';
 
 const CustomerPolicyAgreement = () => {
 
-    const { PageCount , _NextPage } = useContext(CustomerContext);
+    const { PageCount , _NextPage , InitializeSession } = useContext(CustomerContext);
 
     if(PageCount !== 1)
         return null;
@@ -22,7 +22,7 @@ const CustomerPolicyAgreement = () => {
                         Your privacy is important to us. It is our policy to respect your privacy regarding any information we may collect from you across our Web Application, https://github.com/restauMS/web-based-restaurant-management-system, and other sites we own and operate.
                         
                         We only ask for personal information when we truly need it to provide a service to you. We collect it by fair and lawful means, with your knowledge and consent. We also let you know why we’re collecting it and how it will be used.
-            
+                        
                         We only retain collected information for as long as necessary to provide you with your requested service. What data we store, we’ll protect within commercially acceptable means to prevent loss and theft, as well as unauthorized access, disclosure, copying, use or modification.
                         
                         We don’t share any personally identifying information publicly or with third-parties, except when required to by law.
@@ -40,12 +40,13 @@ const CustomerPolicyAgreement = () => {
                     </p>
                 </div>
                 <Button
-                isButtonContrast = {true}
-                ButtonContent = 'I understand'
-                ButtonFunction = {(e) => {
-                    e.preventDefault();
-                    _NextPage();
-                }}
+                    isButtonContrast = {true}
+                    ButtonContent = 'I understand'
+                    ButtonFunction = {(e) => {
+                        e.preventDefault();
+                        InitializeSession(true);
+                        _NextPage();
+                    }}
                 />
             </div>
         </div>
