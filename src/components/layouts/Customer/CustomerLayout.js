adfe1efa9c3164edf1390455cba61e2909127786
Sean Christian Lozana <lozanasc@gmail.com>
2021-06-12 20:50:14 +0800

    Added beforeload function to stop user from refreshing

--- src/components/layouts/Customer/CustomerLayout.js
+++ src/components/layouts/Customer/CustomerLayout.js
@@ -5,6 +5,14 @@ import './style/CustomerLayout.scss';
 
 
 export const CustomerOrder = () => {
+
+    window.addEventListener('beforeunload', function (e) {
+        // Cancel the event
+        e.preventDefault(); // If you prevent default behavior in Mozilla Firefox prompt will always be shown
+        // Chrome requires returnValue to be set
+        e.returnValue = '';
+      });
+
     return (
         <CustomerProvider>
             <div className="CustomerLayoutContainer">
