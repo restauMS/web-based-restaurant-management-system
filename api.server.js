01eb217bc1808bf044759b775a972066a0995680
Sean Christian Lozana <lozanasc@gmail.com>
2021-06-24 00:41:16 +0800

    Uncommented line of code for production

--- api.server.js
+++ api.server.js
@@ -11,15 +11,10 @@ const Worker = require('./routes/worker/routes.worker');
 const Admin = require('./routes/admin/routes.admin');
 const Inventory = require('./routes/inventory/routes.inventory');
 
-/* 
-    ! Not needed atm
-    ! Use only for Deployment/Production
-    ! To serve static files via Express
-    app.use(express.static(path.join(__dirname, 'build')));
-    app.get('/', (req,res) => {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'))
-    });
-*/
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+});
 
 app.use('/API/Customer', Customer);
 app.use('/API/Worker', Worker);
