578e99b79a001f7bdc00f630fa3224763bd0c2e3
Sean Christian Lozana <lozanasc@gmail.com>
2021-06-24 17:49:33 +0800

    stuff added, for fix

--- api.server.js
+++ api.server.js
@@ -13,16 +13,19 @@ const Worker = require('./routes/worker/routes.worker');
 const Admin = require('./routes/admin/routes.admin');
 const Inventory = require('./routes/inventory/routes.inventory');
 
-app.use(express.static(path.join(__dirname, 'restoms-client/build')));
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, 'restoms-client/build', 'index.html'));
-});
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, 'restoms-client/build')));
+    app.get('/', (req,res) => {
+        res.sendFile(path.join(__dirname, 'restoms-client/build', 'index.html'));
+    });
+}
+
 
 app.use('/API/Customer', Customer);
 app.use('/API/Worker', Worker);
 app.use('/API/Admin', Admin);
 app.use('/API/Inventory', Inventory);
 
-app.listen(PORT, () => {
+app.listen(PORT, (Request, Response) => {
     console.log(`Server is running at port = ${PORT}`);
 });
